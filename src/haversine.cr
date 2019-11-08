55390332139d79db8e25b29cd2cf37026eb44216
Anton Maminov <anton.linux@gmail.com>
2019-11-08 15:47:24 +0200

    extend self

--- src/haversine.cr
+++ src/haversine.cr
@@ -5,12 +5,14 @@ require "./haversine/*"
 #
 # https://en.wikipedia.org/wiki/Haversine_formula
 module Haversine
+  extend self
+
   alias Number = Int32 | Float32 | Float64
 
   RAD_PER_DEG = Math::PI / 180
 
   # Calculates the haversine distance between two locations using latitude and longitude.
-  def self.distance(lat1 : Number, lon1 : Number, lat2 : Number, lon2 : Number) : Haversine::Distance
+  def distance(lat1 : Number, lon1 : Number, lat2 : Number, lon2 : Number) : Haversine::Distance
     dlon = lon2 - lon1
     dlat = lat2 - lat1
 
@@ -21,18 +23,18 @@ module Haversine
   end
 
   # :ditto:
-  def self.distance(coord1 : Array(Number), coord2 : Array(Number)) : Haversine::Distance
+  def distance(coord1 : Array(Number), coord2 : Array(Number)) : Haversine::Distance
     lat1, lon1 = coord1
     lat2, lon2 = coord2
 
     distance(lat1, lon1, lat2, lon2)
   end
 
-  private def self.calc(dlat : Number, lat1 : Number, lat2 : Number, dlon : Number) : Number
+  private def calc(dlat : Number, lat1 : Number, lat2 : Number, dlon : Number) : Number
     (Math.sin(rpd(dlat) / 2)) ** 2 + Math.cos(rpd(lat1)) * Math.cos((rpd(lat2))) * (Math.sin(rpd(dlon) / 2)) ** 2
   end
 
-  private def self.rpd(num : Number) : Number
+  private def rpd(num : Number) : Number
     num * RAD_PER_DEG
   end
 end
