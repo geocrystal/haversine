1a4f4fd0ba463150ae58698f8da49b1ffef35641
Anton Maminov <anton.linux@gmail.com>
2019-11-08 15:36:34 +0200

    add more types

--- src/haversine.cr
+++ src/haversine.cr
@@ -7,8 +7,10 @@ require "./haversine/*"
 module Haversine
   RAD_PER_DEG = Math::PI / 180
 
+  alias Number = Int32 | Float32 | Float64
+
   # Calculates the haversine distance between two locations using latitude and longitude.
-  def self.distance(lat1 : Float64, lon1 : Float64, lat2 : Float64, lon2 : Float64)
+  def self.distance(lat1 : Number, lon1 : Number, lat2 : Number, lon2 : Number) : Haversine::Distance
     dlon = lon2 - lon1
     dlat = lat2 - lat1
 
@@ -19,18 +21,18 @@ module Haversine
   end
 
   # ditto
-  def self.distance(cord1 : Array(Float64), cord2 : Array(Float64))
+  def self.distance(cord1 : Array(Number), cord2 : Array(Number)) : Haversine::Distance
     lat1, lon1 = cord1
     lat2, lon2 = cord2
 
     distance(lat1, lon1, lat2, lon2)
   end
 
-  private def self.calc(dlat, lat1, lat2, dlon)
+  private def self.calc(dlat : Number, lat1 : Number, lat2 : Number, dlon : Number) : Number
     (Math.sin(rpd(dlat) / 2))**2 + Math.cos(rpd(lat1)) * Math.cos((rpd(lat2))) * (Math.sin(rpd(dlon) / 2))**2
   end
 
-  private def self.rpd(num)
+  private def self.rpd(num : Number) : Number
     num * RAD_PER_DEG
   end
 end
