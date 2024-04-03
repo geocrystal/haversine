0923354b9b0b384a8aea77b15a99f0ba4c5b756d
Anton Maminov <anton.linux@gmail.com>
2024-04-03 12:30:21 +0300

    set Earth radius to 6371008.8

--- src/haversine/distance.cr
+++ src/haversine/distance.cr
@@ -2,39 +2,43 @@ module Haversine
   class Distance
     include Comparable(self)
 
-    GREAT_CIRCLE_RADIUS_KILOMETERS     = 6371
-    GREAT_CIRCLE_RADIUS_METERS         = GREAT_CIRCLE_RADIUS_KILOMETERS * 1000
-    GREAT_CIRCLE_RADIUS_MILES          = 3956
-    GREAT_CIRCLE_RADIUS_FEET           = GREAT_CIRCLE_RADIUS_MILES * 5280
-    GREAT_CIRCLE_RADIUS_NAUTICAL_MILES = GREAT_CIRCLE_RADIUS_MILES / 1.15078
+    EARTH_RADIUS = 6371008.8
 
-    property great_circle_distance
+    FACTORS = {
+      kilometers:    EARTH_RADIUS / 1000,
+      meters:        EARTH_RADIUS,
+      miles:         EARTH_RADIUS / 1609.344,
+      feet:          EARTH_RADIUS * 3.28084,
+      nauticalmiles: EARTH_RADIUS / 1852,
+    }
 
-    def initialize(@great_circle_distance : Number)
+    property distance
+
+    def initialize(@distance : Number)
     end
 
     def to_kilometers : Number
-      @great_circle_distance * GREAT_CIRCLE_RADIUS_KILOMETERS
+      @distance * FACTORS[:kilometers]
     end
 
     def to_meters : Number
-      @great_circle_distance * GREAT_CIRCLE_RADIUS_METERS
+      @distance * FACTORS[:meters]
     end
 
     def to_miles : Number
-      @great_circle_distance * GREAT_CIRCLE_RADIUS_MILES
+      @distance * FACTORS[:miles]
     end
 
     def to_nautical_miles : Number
-      @great_circle_distance * GREAT_CIRCLE_RADIUS_NAUTICAL_MILES
+      @distance * FACTORS[:nauticalmiles]
     end
 
     def to_feet : Number
-      @great_circle_distance * GREAT_CIRCLE_RADIUS_FEET
+      @distance * FACTORS[:feet]
     end
 
     def <=>(other : Haversine::Distance)
-      great_circle_distance <=> other.great_circle_distance
+      distance <=> other.distance
     end
   end
 end
