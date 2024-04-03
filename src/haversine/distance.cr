f91cbb529aadb958e9fa195efd87a93510526109
Anton Maminov <anton.linux@gmail.com>
2024-04-03 12:36:47 +0300

    refactor code

--- src/haversine/distance.cr
+++ src/haversine/distance.cr
@@ -5,11 +5,11 @@ module Haversine
     EARTH_RADIUS = 6371008.8
 
     FACTORS = {
-      kilometers:    EARTH_RADIUS / 1000,
-      meters:        EARTH_RADIUS,
-      miles:         EARTH_RADIUS / 1609.344,
-      feet:          EARTH_RADIUS * 3.28084,
-      nauticalmiles: EARTH_RADIUS / 1852,
+      kilometers:     EARTH_RADIUS / 1000,
+      meters:         EARTH_RADIUS,
+      miles:          EARTH_RADIUS / 1609.344,
+      feet:           EARTH_RADIUS * 3.28084,
+      nautical_miles: EARTH_RADIUS / 1852,
     }
 
     property distance
@@ -17,25 +17,11 @@ module Haversine
     def initialize(@distance : Number)
     end
 
-    def to_kilometers : Number
-      @distance * FACTORS[:kilometers]
-    end
-
-    def to_meters : Number
-      @distance * FACTORS[:meters]
-    end
-
-    def to_miles : Number
-      @distance * FACTORS[:miles]
-    end
-
-    def to_nautical_miles : Number
-      @distance * FACTORS[:nauticalmiles]
-    end
-
-    def to_feet : Number
-      @distance * FACTORS[:feet]
-    end
+    {% for factor in ["meters", "kilometers", "miles", "nautical_miles", "feet"] %}
+      def to_{{factor.id}} : Number
+        @distance * FACTORS[:{{factor.id}}]
+      end
+    {% end %}
 
     def <=>(other : Haversine::Distance)
       distance <=> other.distance
