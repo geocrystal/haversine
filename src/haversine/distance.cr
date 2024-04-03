c194e4ad55048234005c54e573182f0c67349617
Anton Maminov <anton.linux@gmail.com>
2024-04-03 16:21:02 +0300

    add more units

--- src/haversine/distance.cr
+++ src/haversine/distance.cr
@@ -4,12 +4,25 @@ module Haversine
 
     EARTH_RADIUS = 6371008.8
 
+    # Unit of measurement factors using a spherical (non-ellipsoid) earth radius.
+    #
+    # Keys are the name of the unit, values are the number of that unit in a single radians
     FACTORS = {
-      kilometers:     EARTH_RADIUS / 1000,
-      meters:         EARTH_RADIUS,
-      miles:          EARTH_RADIUS / 1609.344,
+      centimeters:    EARTH_RADIUS * 100,
+      centimetres:    EARTH_RADIUS * 100,
+      degrees:        360 / (2 * Math::PI),
       feet:           EARTH_RADIUS * 3.28084,
+      inches:         EARTH_RADIUS * 39.37,
+      kilometers:     EARTH_RADIUS / 1000,
+      kilometres:     EARTH_RADIUS / 1000,
+      meters:         EARTH_RADIUS,
+      metres:         EARTH_RADIUS,
+      miles:          EARTH_RADIUS / 1609.344,
+      millimeters:    EARTH_RADIUS * 1000,
+      millimetres:    EARTH_RADIUS * 1000,
       nautical_miles: EARTH_RADIUS / 1852,
+      radians:        1,
+      yards:          EARTH_RADIUS * 1.0936,
     }
 
     property distance
@@ -17,7 +30,7 @@ module Haversine
     def initialize(@distance : Number)
     end
 
-    {% for factor in ["meters", "kilometers", "miles", "nautical_miles", "feet"] %}
+    {% for factor in FACTORS.keys %}
       def to_{{factor.id}} : Number
         @distance * FACTORS[:{{factor.id}}]
       end
